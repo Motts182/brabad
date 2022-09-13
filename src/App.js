2c30b110ab2742dceaf53992b2250348e7e45f93
motts182 <mnd.matias.diaz@gmail.com>
2022-09-13 10:32:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+import {useState,useEffect} from 'react'
+import Frases from './components/Frases'
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: --webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #255 0 255;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
+
+  const [frase, setFrase] = useState({})
+
+  const consultarAPI = async () => {
+
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+
+    const frase = await api.json()
+
+    setFrase(frase[0]);
+  }
+
+  useEffect(() => {
+    consultarAPI();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Frases
+      frase={frase}
+      >
+      </Frases>
+
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
